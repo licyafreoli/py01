9896cc31c95d3421d7f57057d3017667ef816459
Rossana Lícya <168486966+licyafreoli@users.noreply.github.com>
2024-11-01 17:14:25 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+contato = {}
+
+contato['nome'] = input("Digite o nome do contato: ")
+contato['telefone'] = input("Digite o número de telefone: ")
+contato['email'] = input("Digite o endereço de email: ")
+
+print("\nInformações do Contato:")
+print(f"Nome: {contato['nome']}")
+print(f"Telefone: {contato['telefone']}")
+print(f"Email: {contato['email']}")
